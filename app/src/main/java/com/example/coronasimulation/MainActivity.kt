aa24a7f686c861e7cf0838d044c24d118436092d
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2020-06-23 22:15:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coronasimulation/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.coronasimulation
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        // setup the nav controller
+        val navController = getNavController()
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    // https://stackoverflow.com/questions/59275009/fragmentcontainerview-using-findnavcontroller
+    // workaround for https://issuetracker.google.com/issues/142847973
+    private fun getNavController(): NavController {
+        val navFragment: Fragment? = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        check(navFragment is NavHostFragment) {
+            ("Activity $this does not have a NavHostFragment")
+        }
+        return (navFragment as NavHostFragment?)!!.navController
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            //R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
