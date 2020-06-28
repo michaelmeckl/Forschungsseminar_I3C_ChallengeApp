76343d84e0c41bf29d4e91d6e56ae39aaf7bc76a
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2020-06-28 20:55:02 +0200

    Add fuel and gson dependencies and perform test api calls

---
+++ app/src/main/java/com/example/coronasimulation/ApiService.kt
@@ -0,0 +1,61 @@
+package com.example.coronasimulation
+
+import com.example.coronasimulation.model.CoronaStatistics
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
+
+object ApiService {
+
+
+
+    init {
+        // set base path for all requests
+        //FuelManager.instance.basePath = "https://disease.sh/v2"
+    }
+
+    //TODO: how to return value, passing the viewmodel is bad practice but returning is not possible either.
+    fun fetchCurrentStatistics(country: String) {
+        // deserialize objects with custom deserializer
+        Fuel.get("https://disease.sh/v2/countries/${country}")
+            .responseObject(CoronaStatistics.Deserializer()) { _, _, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        // Failed request
+                        val ex = result.getException()
+                        println(ex)
+                    }
+                    is Result.Success -> {
+                        // Successful request
+                        val data = result.get()
+                    }
+                }
+            }
+    }
+
+    /*
+    fun makeRequest(): Triple<Response, String, Body> {
+
+        val httpAsync = Fuel.get("https://disease.sh/v2/all")
+            .responseJson { request, response, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        // Failed request
+                        val ex = result.getException()
+                        println(ex)
+                    }
+                    is Result.Success -> {
+                        // Successful request
+                        val data = result.get().content
+                        val test = result.value.obj()
+                        println(data)
+                        println(test)
+                    }
+                }
+            }
+
+        //val response = httpAsync.awaitResponse(Response)
+        val response = httpAsync.join()
+        return Triple(response, response.responseMessage, response.body())
+    }
+    */
+}
--- app/src/main/java/com/example/coronasimulation/ui/news/NewsViewModel.kt
+++ app/src/main/java/com/example/coronasimulation/ui/news/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.coronasimulation.ui.news
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.coronasimulation.model.CoronaStatistics
 
 class NewsViewModel : ViewModel() {
 
@@ -10,4 +11,6 @@ class NewsViewModel : ViewModel() {
         value = "This is news Fragment"
     }
     val text: LiveData<String> = _text
+
+    val statistics: MutableLiveData<CoronaStatistics> by lazy { MutableLiveData<CoronaStatistics>() }
 }
