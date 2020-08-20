2ca464cc08459dd2fe162a35a1214481d8152600
shedoufleim <36131400+shedoufleim@users.noreply.github.com>
2020-08-20 21:53:22 +0200

    Improve UI of create challenge fragment

--- app/src/main/java/com/example/challengecovid/ui/CreateChallengeFragment.kt
+++ app/src/main/java/com/example/challengecovid/ui/CreateChallengeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +14,7 @@ import com.example.challengecovid.R
 import com.example.challengecovid.model.Challenge
 import com.example.challengecovid.viewmodels.ChallengeViewModel2
 import com.example.challengecovid.viewmodels.ChallengesViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_create_new_challenge.*
 import timber.log.Timber
 import java.util.*
@@ -35,9 +37,9 @@ class CreateChallengeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //get the spinner from the xml.
         challengeViewModel2 = ViewModelProvider(this).get(ChallengeViewModel2::class.java)
 
+        // init adapter for difficulty spinner
         ArrayAdapter.createFromResource(
             requireContext(),
             R.array.difficulties_challenges,
@@ -48,7 +50,36 @@ class CreateChallengeFragment : Fragment() {
             // Apply the adapter to the spinner
             spinner_create_new_challenge.adapter = adapter
         }
+
+        // init adapter for icon spinner
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.icons_challenges,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner_icon_create_new_challenge.adapter = adapter
+        }
+
+        // Toast showing more info for duration edittext
+        info_duration_create_challenge.setOnClickListener {
+//            Snackbar.make(requireView(), "Gib an, wie viele Tage lang du deine Challenge machen willst. Lass das Feld leer, wenn die Challenge unendlich lang drin bleiben soll", Snackbar.LENGTH_LONG).show()
+            Toast.makeText(
+                requireContext(),
+                "Gib an, wie viele Tage lang du deine Challenge machen willst. Lass das Feld leer, wenn die Challenge unendlich lang drin bleiben soll",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
         button_submit_create_new_challenge.setOnClickListener {
+            if (name_create_new_challenge.text.toString().isBlank()) {
+                layout_name_create_new_challenge.error = "Braucht nen namen alter"
+                return@setOnClickListener
+            }
+            //TODO: Kategorien/Icons hinzufügen
+            layout_name_create_new_challenge.error = ""
             val randomNum = Random.nextInt()
             Timber.tag("randomNumber").d(randomNum.toString())
 
