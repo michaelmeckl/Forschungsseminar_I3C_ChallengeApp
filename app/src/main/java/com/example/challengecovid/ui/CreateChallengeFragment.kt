c6ad56da283bd773ad384c183623a9f53ff2b861
shedoufleim <36131400+shedoufleim@users.noreply.github.com>
2020-08-20 17:53:42 +0200

    Add dummy challenge on create challenge button click

--- app/src/main/java/com/example/challengecovid/ui/CreateChallengeFragment.kt
+++ app/src/main/java/com/example/challengecovid/ui/CreateChallengeFragment.kt
@@ -7,11 +7,24 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.example.challengecovid.R
+import com.example.challengecovid.model.Challenge
+import com.example.challengecovid.viewmodels.ChallengeViewModel2
+import com.example.challengecovid.viewmodels.ChallengesViewModel
 import kotlinx.android.synthetic.main.fragment_create_new_challenge.*
+import timber.log.Timber
+import java.util.*
+import kotlin.random.Random
 
 
 class CreateChallengeFragment : Fragment() {
+
+    private var root: View? = null
+    private val challengesViewModel: ChallengesViewModel by viewModels()
+    private lateinit var challengeViewModel2: ChallengeViewModel2
+
     fun CreateChallengeFragment() {
 
     }
@@ -23,7 +36,7 @@ class CreateChallengeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //get the spinner from the xml.
-
+        challengeViewModel2 = ViewModelProvider(this).get(ChallengeViewModel2::class.java)
 
         ArrayAdapter.createFromResource(
             requireContext(),
@@ -36,7 +49,28 @@ class CreateChallengeFragment : Fragment() {
             spinner_create_new_challenge.adapter = adapter
         }
         button_submit_create_new_challenge.setOnClickListener {
+            val randomNum = Random.nextInt()
+            Timber.tag("randomNumber").d(randomNum.toString())
 
+            val dummyChallenge = Challenge(
+                randomNum,
+                "New dummy challenge",
+                "A new challenge was created! Good job!",
+                R.drawable.ic_trophy,
+                4,
+                "easy",
+                2f,
+                Date().time
+            )
+
+            /*
+            val job = CoroutineScope(Dispatchers.Main).launch {
+                withContext(Dispatchers.IO) {
+                    dataSource.insert(dummyChallenge)
+                }
+            }
+            */
+            challengeViewModel2.insert(dummyChallenge)
 
             // Navigate back to ChallengesFragment
             val newFragment: Fragment = ChallengesFragment()
