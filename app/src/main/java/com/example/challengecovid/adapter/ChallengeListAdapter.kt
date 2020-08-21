01585ce345c95a4771ba85e4a94ffbef5c7a59d3
shedoufleim <36131400+shedoufleim@users.noreply.github.com>
2020-08-21 21:59:17 +0200

    Add description to challenge item

--- app/src/main/java/com/example/challengecovid/adapter/ChallengeListAdapter.kt
+++ app/src/main/java/com/example/challengecovid/adapter/ChallengeListAdapter.kt
@@ -21,6 +21,7 @@ class ChallengeListAdapter internal constructor(
         val challengeItemView: TextView = itemView.findViewById(R.id.name_challenge)
         val challengeItemXP: TextView = itemView.findViewById(R.id.xp_challenge)
         val challengeItemIcon: ImageView = itemView.findViewById(R.id.icon_challenge)
+        val challengeItemDescription: TextView = itemView.findViewById(R.id.description_challenge)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChallengeViewHolder {
@@ -32,6 +33,7 @@ class ChallengeListAdapter internal constructor(
         val current = challenges[position]
         holder.challengeItemView.text = current.title
         holder.challengeItemXP.text = String.format("%s XP", current.points.toString())
+        holder.challengeItemDescription.text = current.description
 //        TODO: Sollte so gehen später
 //        holder.challengeItemIcon.background = current.iconPath
     }
