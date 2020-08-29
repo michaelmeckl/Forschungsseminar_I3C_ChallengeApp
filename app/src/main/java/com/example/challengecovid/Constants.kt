7f4e778c67e03e19b532a93df6e4733ff4d47791
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2020-08-29 15:46:14 +0200

    Save the switch state in shared prefs

--- app/src/main/java/com/example/challengecovid/Constants.kt
+++ app/src/main/java/com/example/challengecovid/Constants.kt
@@ -3,4 +3,5 @@ package com.example.challengecovid
 object Constants {
     const val SHARED_PREFS_NAME = "challengeCovidSharedPrefs"
     const val PREFS_USER_ID = "CurrentUserId"
+    const val PREFS_SWITCH_STATE = "PublicPrivateSwitchState"
 }
