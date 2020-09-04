e5223bd3e9716e7b98269cfe488bf98fcc8c2c02
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2020-09-04 23:57:07 +0200

    Fix visibility issues and cleanup a little bit in challenge detail

--- app/src/main/java/com/example/challengecovid/ui/ChallengeDetailFragment.kt
+++ app/src/main/java/com/example/challengecovid/ui/ChallengeDetailFragment.kt
@@ -73,99 +73,80 @@ class ChallengeDetailFragment : Fragment() {
 
         isEditable = sharedPrefs.getBoolean(Constants.PREFS_IS_CHALLENGE_BY_THIS_USER + id, false)
 
-        //FIXME: statt alles ständig auf hidden oder visible zu setzen wäre hier ein neues eigenes Layout deutlich besser
+        //TODO: statt alles ständig auf hidden oder visible zu setzen wäre hier wahrscheinlich ein neues eigenes Layout besser
         if (type == ChallengeType.SYSTEM_CHALLENGE || !isEditable) {
-            // hide the option to publish for system challenges
-            publish_switch.visibility = View.GONE
-
-            challenge_detail_start_editing.visibility = View.GONE
-            challenge_detail_relativelayout_online.visibility = View.GONE
-            challenge_detail_relativelayout_offline.visibility = View.GONE
+            // hide the option to publish and to edit for system challenges
+            setVisibility(
+                publish_switch,
+                challenge_detail_start_editing,
+                challenge_detail_relativelayout_online,
+                challenge_detail_relativelayout_offline,
+                visible = false
+            )
         }
 
         challenge_detail_start_editing.setOnClickListener {
-            challenge_detail_title.visibility = View.GONE
-            challenge_detail_description.visibility = View.GONE
-            challenge_detail_difficulty.visibility = View.GONE
-            challenge_detail_start_editing.visibility = View.GONE
 
-            challenge_detail_header_edit.visibility = View.VISIBLE
-            challenge_detail_stop_editing.visibility = View.VISIBLE
-            layout_challenge_detail_title_edit.visibility = View.VISIBLE
+            setVisibility(
+                challenge_detail_title,
+                challenge_detail_description,
+                challenge_detail_difficulty,
+                challenge_detail_start_editing,
+                publish_switch,
+                visible = false
+            )
+            setVisibility(
+                challenge_detail_header_edit,
+                challenge_detail_stop_editing,
+                layout_challenge_detail_title_edit,
+                layout_challenge_detail_description_edit,
+                visible = true
+            )
+
             challenge_detail_title_edit.hint = title
-            layout_challenge_detail_description_edit.visibility = View.VISIBLE
             challenge_detail_description_edit.hint = description
 
             if (description.isBlank()) {
                 challenge_detail_description_edit.hint = "Beschreibung"
             }
 
-            challenge_detail_spinner_difficulties_edit.visibility = View.VISIBLE
+            setVisibility(challenge_detail_spinner_difficulties_edit, visible = true)
             when (difficulty) {
                 Difficulty.LEICHT.toString() -> challenge_detail_spinner_difficulties_edit.setSelection(0)
                 Difficulty.MITTEL.toString() -> challenge_detail_spinner_difficulties_edit.setSelection(1)
                 Difficulty.SCHWER.toString() -> challenge_detail_spinner_difficulties_edit.setSelection(2)
             }
-            challenge_detail_button_submit_edit.visibility = View.VISIBLE
+            setVisibility(challenge_detail_button_submit_edit, visible = true)
 
         }
 
         challenge_detail_stop_editing.setOnClickListener {
-            challenge_detail_title.visibility = View.VISIBLE
-            challenge_detail_description.visibility = View.VISIBLE
-            challenge_detail_difficulty.visibility = View.VISIBLE
-            challenge_detail_start_editing.visibility = View.VISIBLE
+            setVisibility(
+                challenge_detail_title,
+                challenge_detail_description,
+                challenge_detail_difficulty,
+                challenge_detail_start_editing,
+                publish_switch,
+                visible = true
+            )
 
-            challenge_detail_header_edit.visibility = View.GONE
-            challenge_detail_stop_editing.visibility = View.GONE
-            layout_challenge_detail_title_edit.visibility = View.GONE
-            layout_challenge_detail_description_edit.visibility = View.GONE
-            challenge_detail_spinner_difficulties_edit.visibility = View.GONE
-            challenge_detail_button_submit_edit.visibility = View.GONE
+            setVisibility(
+                challenge_detail_header_edit,
+                challenge_detail_stop_editing,
+                layout_challenge_detail_title_edit,
+                layout_challenge_detail_description_edit,
+                challenge_detail_spinner_difficulties_edit,
+                challenge_detail_button_submit_edit,
+                visible = false
+            )
+
             if (description.isBlank()) {
-                challenge_detail_description.visibility = View.GONE
+                setVisibility(challenge_detail_description, visible = false)
             }
         }
 
         challenge_detail_button_submit_edit.setOnClickListener {
-
-            //FIXME: lieber den Nutzer die felder bearbeiten lassen, die er auch bearbeiten will, statt alles wieder leer zu setzen!!
-            // und auf keinen Fall verlangen dass der Titel neu eingegeben werden muss -> miese UX
-            // stattdessen überprüfen, was sich verändert hat (vergleich if alterTitel != neuer Titel) und nur das updaten
-            val newTitle = challenge_detail_title_edit.text.toString()
-            /*
-            if (newTitle.isBlank()) {
-                layout_challenge_detail_title_edit.error = "Name erforderlich"
-                return@setOnClickListener
-            }*/
-            val newDescription = challenge_detail_description_edit.text.toString()
-            val difficulties = resources.getStringArray(R.array.difficulties_challenges)
-            val newDifficulty: Difficulty = when (challenge_detail_spinner_difficulties_edit.selectedItem) {
-                difficulties[0] -> Difficulty.LEICHT
-                difficulties[1] -> Difficulty.MITTEL
-                difficulties[2] -> Difficulty.SCHWER
-                else -> Difficulty.LEICHT
-            }
-
-            val userid = sharedPrefs.getString(Constants.PREFS_USER_ID, "") ?: ""
-
-            val newUserChallenge = UserChallenge(
-                challengeId = id,
-                title = newTitle,
-                description = newDescription,
-                difficulty = newDifficulty,
-                type = ChallengeType.USER_CHALLENGE,
-                completed = completed,
-                isPublic = false,
-                creatorId = userid
-            )
-            Timber.d("Geänderte Challenge: " + newUserChallenge.toString())
-
-            publish_switch.isChecked = false
-
-            overviewViewModel.updateChallenge(newUserChallenge)
-
-            requireActivity().findNavController(R.id.nav_host_fragment).popBackStack()
+            saveUpdatedChallenge()
         }
 
     }
@@ -189,7 +170,8 @@ class ChallengeDetailFragment : Fragment() {
 
 
         // get the saved switch state and set it
-        sharedPrefs = activity?.getSharedPreferences(Constants.SHARED_PREFS_NAME, AppCompatActivity.MODE_PRIVATE) ?: return
+        sharedPrefs =
+            activity?.getSharedPreferences(Constants.SHARED_PREFS_NAME, AppCompatActivity.MODE_PRIVATE) ?: return
         val switchState = sharedPrefs.getBoolean(Constants.PREFS_SWITCH_STATE + id, false)
         publish_switch.isChecked = switchState
         setStatus(switchState)
@@ -216,11 +198,11 @@ class ChallengeDetailFragment : Fragment() {
 
         //update the public status of the challenge
         if (isChecked) {
-            challenge_detail_start_editing.visibility = View.GONE
+            setVisibility(challenge_detail_start_editing, visible = false)
             overviewViewModel.updatePublicStatus(id, isChecked)
         } else {
             if (type == ChallengeType.USER_CHALLENGE && isEditable) {
-                challenge_detail_start_editing.visibility = View.VISIBLE
+                setVisibility(challenge_detail_start_editing, visible = true)
             }
         }
 
@@ -228,7 +210,7 @@ class ChallengeDetailFragment : Fragment() {
 
 //          TODO: Hier wird noch primitiv eine Progressbar für 2s eingeblendet und dann der jeweils andere state angezeigt, obwohl nicht geschaut wird ob success oder failure
 
-        challenge_detail_progressbar.visibility = View.VISIBLE
+        setVisibility(challenge_detail_progressbar, visible = true)
         requireActivity().window.setFlags(
             WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
             WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
@@ -241,30 +223,83 @@ class ChallengeDetailFragment : Fragment() {
         Handler().postDelayed(this::switchStatus, 2000)
     }
 
-    //FIXME: man kann das veröffentlichen layout noch sehen wenn im bearbeitungsmodus! Das führt ständig zu abstürzen!!!
+    //FIXME: lieber den Nutzer die felder bearbeiten lassen, die er auch bearbeiten will, statt alles wieder leer zu setzen!!
+    // und auf keinen Fall verlangen dass der Titel neu eingegeben werden muss -> miese UX
+    // stattdessen überprüfen, was sich verändert hat (vergleich if alterTitel != neuer Titel) und nur das updaten
+    private fun saveUpdatedChallenge() {
+        val newTitle = challenge_detail_title_edit.text.toString()
+        /*
+        if (newTitle.isBlank()) {
+            layout_challenge_detail_title_edit.error = "Name erforderlich"
+            return@setOnClickListener
+        }*/
+        val newDescription = challenge_detail_description_edit.text.toString()
+        val difficulties = resources.getStringArray(R.array.difficulties_challenges)
+        val newDifficulty: Difficulty = when (challenge_detail_spinner_difficulties_edit.selectedItem) {
+            difficulties[0] -> Difficulty.LEICHT
+            difficulties[1] -> Difficulty.MITTEL
+            difficulties[2] -> Difficulty.SCHWER
+            else -> Difficulty.LEICHT
+        }
+
+        val userid = sharedPrefs.getString(Constants.PREFS_USER_ID, "") ?: ""
+
+        val newUserChallenge = UserChallenge(
+            challengeId = id,
+            title = newTitle,
+            description = newDescription,
+            difficulty = newDifficulty,
+            type = ChallengeType.USER_CHALLENGE,
+            completed = completed,
+            isPublic = false,
+            creatorId = userid
+        )
+        Timber.d("Geänderte Challenge: " + newUserChallenge.toString())
+
+        publish_switch.isChecked = false
+
+        overviewViewModel.updateChallenge(newUserChallenge)
+
+        requireActivity().findNavController(R.id.nav_host_fragment).popBackStack()
+    }
+
+    /**
+     * Util-Method to change visibility of the given views based on the given parameter "visible" (GONE if false, VISIBLE if true)
+     */
+    private fun setVisibility(vararg views: View, visible: Boolean) {
+        val visibilityStatus = if (visible) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+
+        for (v in views) {
+            v.visibility = visibilityStatus
+        }
+    }
 
     private fun setStatus(switchState: Boolean) {
         if (switchState) {
-            challenge_detail_relativelayout_offline.visibility = View.GONE
-            challenge_detail_relativelayout_online.visibility = View.VISIBLE
-            challenge_detail_start_editing.visibility = View.GONE
+            setVisibility(challenge_detail_relativelayout_online, visible = true)
+            setVisibility(challenge_detail_relativelayout_offline, challenge_detail_start_editing, visible = false)
         } else {
-            challenge_detail_relativelayout_online.visibility = View.GONE
-            challenge_detail_relativelayout_offline.visibility = View.VISIBLE
-            challenge_detail_start_editing.visibility = View.VISIBLE
+            setVisibility(challenge_detail_relativelayout_offline, challenge_detail_start_editing, visible = true)
+            setVisibility(challenge_detail_relativelayout_online, visible = false)
         }
     }
 
     private fun switchStatus() {
-        //TODO: app crasht hier!!!!!!
-        // challenge_detail_relativelayout_offline ist null wenn geedited und dann neu geöffnet
-        //challenge_detail_relativelayout_offline ?: return
+        //TODO: app crasht hier, da challenge_detail_relativelayout_offline gleich null wenn geedited und dann neu geöffnet
+        // -> sollte jetzt aber nicht mehr passieren können
+
+        // bessere if bedingung hier nötig (ohne layout zu referenzieren, das vielleicht nicht sichtbar ist)!
+
         if (challenge_detail_relativelayout_offline.visibility == View.GONE) {
-            challenge_detail_relativelayout_online.visibility = View.GONE
-            challenge_detail_relativelayout_offline.visibility = View.VISIBLE
+            setVisibility(challenge_detail_relativelayout_online, visible = false)
+            setVisibility(challenge_detail_relativelayout_offline, visible = true)
         } else {
-            challenge_detail_relativelayout_offline.visibility = View.GONE
-            challenge_detail_relativelayout_online.visibility = View.VISIBLE
+            setVisibility(challenge_detail_relativelayout_online, visible = true)
+            setVisibility(challenge_detail_relativelayout_offline, visible = false)
         }
         requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
         challenge_detail_progressbar.visibility = View.GONE
